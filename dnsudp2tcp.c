59d20ddb3cf9739af0e8e35a694d1ac7b1c396ac
zfl9 <zfl9.com@gmail.com>
2020-03-03 21:58:52 +0800

    use libev instead of libuv

---
+++ dnsudp2tcp.c
@@ -0,0 +1,19 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <time.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include "libev/ev.h"
+
+// TODO
